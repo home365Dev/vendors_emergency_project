47d509de8d087a72126855ce5649d4672addfad9
peleg <peleg@home365.co>
2022-08-23 17:13:52 +0300

    FIXING ERGENCY TERMS

--- vendors_emergency_projects/src/emergency_checker.py
+++ vendors_emergency_projects/src/emergency_checker.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import config
 
-emergency_terms = ['urgent', 'crisis', 'danger', 'disaster', 'necessity', 'catastrophe', 'catastrof', 'katastrof', 'critical', 'mergnc', 'mergenc', 'emergency', 'immediately', 'flood', 'tornado', 'hurricane', 'tsunami', 'landslide', 'earthquake', 'fire', 'asap', 'ambulance', '911', 'rescue', 'burn', 'bleed', 'choke', 'attack', 'poison', 'robbery', 'vandalism', 'alert', 'explosion', 'bomb', 'shot', 'accident', 'leak']
+emergency_terms = ['urgent', 'crisis', 'danger', 'disaster', 'necessity', 'catastrophe', 'catastrof', 'katastrof', 'critical', 'mergnc', 'mergenc', 'emergency', 'immediately', 'flood', 'tornado', 'hurricane', 'tsunami', 'landslide', 'earthquake', 'fire', 'asap', 'ambulance', '911', 'rescue', 'burn', 'bleed', 'choke', 'attack', 'robbery', 'vandalism', 'explosion', 'leak']
 special_terms = ['water', 'heat']
 negative_terms = ['not', 'no', 'doesnt']
 ## police is no good, since it is converted from the spoken word of "please"
