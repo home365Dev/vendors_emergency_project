9f721fda45d7b59c02bb53cbfe6a73a528ac92a4
NoamHome365 <noam@home365.co>
2022-12-05 13:02:50 +0200

    fix DB writing + add date into table

--- src/vendors_emergency_projects/main.py
+++ src/vendors_emergency_projects/main.py
@@ -32,3 +32,16 @@ async def vendors_emergency_projects(request: Request):
 app.include_router(router, prefix="/vendors_emergency_projects")
 
 logger.info("Starting up")
+
+
+if __name__ == '__main__':
+    body = { "text": "all right , I m probably gon na regret order point , soon possible master bathroom , uh sort valve continually run water toilet bowl . I try I include go Home Depot try replacement um sort flush mechanism like I use . I figure constant run . so end water drain tank fill night long . its drive crazy . not urgent , certainly go , drain trigger refill . um not good house sort chronically run toilet . tub bathroom toilet bathtub machine", 'project_id': 'c5c6ba50-245f-4be1-aab8-5c68dac1bb64'}
+    result_to_dict, project_id_str, input_str = execute(body)
+    result = {
+        'statusCode': 200,
+        'body': result_to_dict
+    }
+    thread = threading.Thread(target=execute_to_db, kwargs={
+        'res_to_dict': result_to_dict, 'id': project_id_str, 'text_of_case': input_str})
+    thread.start()
+    print("done")
